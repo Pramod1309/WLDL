926f779b96018a9eb688e63aa779022fe602ba16
Pramod1309 <pramodpandey13092002@gmail.com>
2026-01-05 02:18:16 +0530

    URL

--- backend/server.py
+++ backend/server.py
@@ -2667,7 +2667,7 @@ app.include_router(api_router)
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["https://koshquest.in", "https://www.koshquest.in", "http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -2678,7 +2678,7 @@ app.add_middleware(
 @app.middleware("http")
 async def add_cors_header(request, call_next):
     response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+    response.headers["Access-Control-Allow-Origin"] = "https://koshquest.in"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
